abd68633e21f92a86bc1a87172c26081df26c6e6
ozaste <edvin.hasic@elev.ga.ntig.se>
2020-08-31 12:04:26 +0200

    yeeeee

--- Pizza/Pizza/Program.cs
+++ Pizza/Pizza/Program.cs
@@ -7,6 +7,23 @@ namespace Pizza
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Niklas");
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+
+            Console.WriteLine("Enter name:");
+
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Your name is: " + Name);
+
+            Console.WriteLine("Are you alive " + Name + "?");
+
+            string alive = Console.ReadLine();
+
+            Console.WriteLine("Alive: " + alive);
+
         }
     }
 }
